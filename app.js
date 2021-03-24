e141f41cc76efd624824bad0d7a6d4f0d76c79fb
OmerBotbol <omerbot2@gmail.com>
2021-03-24 14:44:41 +0200

    create get entry point

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose")
+const env = process.env.NODE_ENV || "production";
+const MONGO_URI =
+  env === "test" ? process.env.TEST_MONGO_URI : process.env.MONGO_URI;
+
+  mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log(`connected to MongoDB - ${env}`);
+
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+  const ticketSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    userEmail: String,
+    done: Boolean,
+    creationTime: Number,
+    labels: Array
+  });
+
+const Ticket = mongoose.model("Ticket", ticketSchema)
 
 app.use(express.static("client/build"));
+
+app.get("/api/tickets", (req, res)=>{
+    const searchText = req.query.searchText
+    Ticket.find().then((data)=>{
+        if(searchText){
+            res.send(data.filter((ticket)=> ticket.title.includes(searchText)))
+        }
+        else{
+            res.send(data)
+        }
+    })
+})
 
 module.exports = app;
