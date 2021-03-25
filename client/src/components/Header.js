8641ce0f84c46276c056f5268d37a95d4d840bb5
OmerBotbol <omerbot2@gmail.com>
2021-03-25 10:17:11 +0200

    add header style

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import '../styles/header.css'
+
 
 function Header(props) {
     return (
         <div>
             <h1>Ticket Manager</h1>
-            <div><span id="hideTicketsCounter">{props.counter}</span> tickets hidden</div>
-            <button id="restoreHideTickets" onClick={()=> props.restoreAll()}>Restore</button>
+            <div id="hide-control">
+                <span id="hideTicketsCounter">{props.counter}</span> tickets hidden
+                <button id="restoreHideTickets" onClick={()=> props.restoreAll()}>Restore</button>
+            </div>
         </div>
     )
 }
