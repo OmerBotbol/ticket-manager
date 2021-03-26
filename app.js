6a236634e3baf823b5ce13c6b7a727f5293ec8d6
OmerBotbol <omerbot2@gmail.com>
2021-03-26 10:36:23 +0300

    create post entry point

--- app.js
+++ app.js
@@ -32,18 +32,19 @@ const MONGO_URI =
 const Ticket = mongoose.model("Ticket", ticketSchema)
 
 app.use(express.static("client/build"));
+app.use(express.json())
 
 app.get("/api/tickets", (req, res)=>{
     const searchText = req.query.searchText;
     Ticket.find().then((data)=>{
         if(searchText){
-            res.send(data.filter((ticket)=> ticket.title.toLowerCase().includes(searchText.toLowerCase())))
+            res.send(data.filter((ticket)=> ticket.title.toLowerCase().includes(searchText.toLowerCase())));
         }
         else{
             res.send(data);
         }
     }).catch((err)=>{
-        res.status(500).send({error: err.message})
+        res.status(500).send({error: err.message});
     })
 })
 
@@ -63,15 +64,33 @@ app.patch("/api/tickets/:ticketid/:isDone", (req, res)=>{
             })
         }
         else{
-            const err = new Error("Invalid 'done' status")
+            const err = new Error("Invalid 'done' status");
             console.log(err.message)
-            res.status(400).send({error: err.message})
+            res.status(400).send({error: err.message});
         }
     }).catch(()=>{
-        const err = new Error("Invalid ID")
+        const err = new Error("Invalid ID");
         console.log(err.message);
-        res.status(400).send({error: err.message})
+        res.status(400).send({error: err.message});
     })
 })
+
+app.post("/api/tickets",((req, res)=>{
+    const dataFromUser = req.body;
+    const newTicket = new Ticket({
+        title: dataFromUser.title,
+        content: dataFromUser.content,
+        userEmail: dataFromUser.userEmail,
+        done: false,
+        creationTime: (new Date().getTime() + 7200000),
+        labels: dataFromUser.labels
+    })
+    newTicket.save().then((data)=>{
+        res.send(data)
+    }).catch((err)=>{
+        console.log(err.message)
+        res.status(500).send({error: err.message})
+    })
+}))
 
 module.exports = app;
